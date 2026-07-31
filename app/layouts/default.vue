f32c2472599c4045ac76763cf2abd8a6d3939337
AliGm12 <aligm1234@yahoo.com>
2026-07-31 22:00:35 +0330

    center main content container

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen max-w-desktop mx-auto bg-[#E9EDF5]">
     <LayoutHeader />
-    <main class="container mx-auto px-4 py-8 max-w-7xl">
+    <main class="container mx-auto px-4 py-8 max-w-7xl flex justify-center">
       <slot />
     </main>
     <LayoutFooter />
